58d14580cc7180b47008a45b14c0889b42e2b4c6
Nate <nate@Nates-MacBook-Pro.local>
2024-06-03 16:46:02 +0300

    page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import "@/styles/globals.css"
 
+export const metadata = {
+  title: {
+    default: "Thest",
+  },
+}
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
